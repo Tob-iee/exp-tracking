57d5849d786bafffa0c8a81f68dd0a744fec0859
tobiee <tochukwunwoke1@gmail.com>
2022-09-12 14:32:34 +0100

    updated train.py

--- src/train.py
+++ src/train.py
@@ -195,21 +195,29 @@ def main():
   search_exp = client.get_experiment_by_name(EXPERIMENT_NAME)
   print(client_list)
 
+  search_exp = None
+  for experiment in client_list:
+    schema = dict(experiment)
+    if schema["name"] == EXPERIMENT_NAME:
+      print(schema["name"])
+      search_exp == True
+      pass
 
-  if search_exp == None:
+
+  if client_list == None or search_exp != True:
     # create and set experiment
     experiment_new = mlflow.create_experiment(EXPERIMENT_NAME)
     client.set_experiment_tag(experiment_new, "CV.framework", "Tensorflow_CV")
-    experiment = client.get_experiment(experiment_new)
+    # experiment = client.get_experiment(experiment_new)
     print("Name: {}".format(experiment.name))
     print("Experiment_id: {}".format(experiment.experiment_id))
     print("Artifact Location: {}".format(experiment.artifact_location))
     print("Tags: {}".format(experiment.tags))
     print("Lifecycle_stage: {}".format(experiment.lifecycle_stage))
 
-  elif search_exp.lifecycle_stage == "active":
+  elif search_exp == True:
     # Set experiment
-    mlflow.set_experiment(experiment_name=EXPERIMENT_NAME)
+    # mlflow.set_experiment(experiment_name=EXPERIMENT_NAME)
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     print("Name: {}".format(experiment.name))
     print("Experiment_id: {}".format(experiment.experiment_id))
@@ -229,43 +237,43 @@ def main():
   mlflow.tensorflow.autolog(every_n_iter=2)
 
   # start experiment tracking runs
-  # with mlflow.start_run(experiment_id=experiment.experiment_id):
+  with mlflow.start_run(experiment_id=experiment.experiment_id):
 
-  run = mlflow.active_run()
-  print(f"run_id: {run.info.run_id}; status: {run.info.status}")
+    run = mlflow.active_run()
+    print(f"run_id: {run.info.run_id}; status: {run.info.status}")
 
-  # Training
-  start_training = time.time()
-  history = model.fit(tfr_dataset,
-            epochs=2, verbose=1)
-  end_training = time.time()
+    # Training
+    start_training = time.time()
+    history = model.fit(tfr_dataset,
+              epochs=2, verbose=1)
+    end_training = time.time()
 
-  training_time = end_training - start_training
+    training_time = end_training - start_training
 
-  mlflow.log_param("learning_rate", 0.0001)
-  mlflow.log_param("optimizer", "Adam")
-  mlflow.log_metric('batchsize', BATCH_SIZE)
-  mlflow.log_metric('training_accuracy', history.history['sparse_categorical_accuracy'][-1])
-  mlflow.log_metric('training_loss', history.history['loss'][-1])
-  mlflow.log_metric('training_time', training_time)
+    mlflow.log_param("learning_rate", 0.0001)
+    mlflow.log_param("optimizer", "Adam")
+    mlflow.log_metric('batchsize', BATCH_SIZE)
+    mlflow.log_metric('training_accuracy', history.history['sparse_categorical_accuracy'][-1])
+    mlflow.log_metric('training_loss', history.history['loss'][-1])
+    mlflow.log_metric('training_time', training_time)
 
-  tfr_testdata = get_dataset(VALID_FILENAMES)
+    tfr_testdata = get_dataset(VALID_FILENAMES)
 
-  start_evaluating = time.time()
-  val_loss, val_accuracy = model.evaluate(tfr_testdata)
+    start_evaluating = time.time()
+    val_loss, val_accuracy = model.evaluate(tfr_testdata)
 
-  end_evaluating = time.time()
-  evaluating_time = end_evaluating - start_evaluating
+    end_evaluating = time.time()
+    evaluating_time = end_evaluating - start_evaluating
 
 
-  mlflow.log_metric('validation_accuracy', val_accuracy)
-  mlflow.log_metric('validation_loss', val_loss)
-  mlflow.log_metric('evaluating_time', evaluating_time)
+    mlflow.log_metric('validation_accuracy', val_accuracy)
+    mlflow.log_metric('validation_loss', val_loss)
+    mlflow.log_metric('evaluating_time', evaluating_time)
 
-  run = mlflow.get_run(run.info.run_id)
-  print(f"run_id: {run.info.run_id}; status: {run.info.status}")
-  print("--")
-  mlflow.end_run()
+    run = mlflow.get_run(run.info.run_id)
+    print(f"run_id: {run.info.run_id}; status: {run.info.status}")
+    print("--")
+    mlflow.end_run()
 
   # Check for any active runs
   print(f"Active run: {mlflow.active_run()}")
